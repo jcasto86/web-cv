e60f28e974e1ef9d88e9965a1cb7677b0077ab54
Javier Casto Jarillo <javier.casto.jarillo@accenture.com>
2023-06-11 14:18:00 +0200

    added voluntary positions

--- src/app/components/pages/voluntary-work/voluntary-work.component.ts
+++ src/app/components/pages/voluntary-work/voluntary-work.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { PreviousNextArrows } from '../../parts/arrows-previous-next-section/arrows-previous-next-section-data.model';
+import { Observable, Subscription } from 'rxjs';
+import { DataService } from 'src/app/data.service';
+import { JobPosition } from '../work-experience/job-position-data.model';
 
 @Component({
   selector: 'app-voluntary-work',
   templateUrl: './voluntary-work.component.html',
   styleUrls: ['./voluntary-work.component.scss'],
 })
-export class VoluntaryWorkComponent implements OnInit {
+export class VoluntaryWorkComponent implements OnDestroy {
   public arrowsData: PreviousNextArrows = {
     previousText: 'Skills',
     routerLinkPrevious: '/skills',
@@ -14,7 +17,94 @@ export class VoluntaryWorkComponent implements OnInit {
     routerLinkNext: '/contact',
   };
 
-  constructor() {}
+  @HostListener('window:keyup.escape') handleKeyUp() {
+    this.closeFormModal()
+  }
 
-  ngOnInit(): void {}
+  /**
+   * Indicates whether the Job Position form modal is open.
+   */
+  public isFormModalOpen: boolean = false;
+
+  /**
+   * Variables that stores all the Job Positions data.
+   */
+  public jobPositionsCardData$?: Observable<JobPosition[]>;
+
+  /**
+   * Indicates the Job Position that is being used to edit.
+   * In add mode, the currentJobPosition is undefined.
+   */
+  public currentJobPosition?: JobPosition;
+
+  /**
+   * Indicates whether the Job Position Form is in edit mode.
+   * If it is false, it would be in add mode.
+   */
+  public editMode: boolean = false;
+
+  /**
+   * Subscriptions.
+   */
+  private subscriptions: Subscription = new Subscription();
+
+  constructor(private dataService: DataService, private cdr: ChangeDetectorRef) {
+    this.jobPositionsCardData$ = this.dataService.getJobPositions()
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  /**
+  * Method to open the Form Modal on Add Mode.
+  */
+  public openFormModal() {
+    this.currentJobPosition = undefined;
+    this.editMode = false;
+    this.isFormModalOpen = true;
+  }
+
+  /**
+   * Method to open the Form Modal on Edit Mode.
+   * @param jobPosition 
+   */
+  public openEditJobPositionModal(jobPosition: JobPosition) {
+    this.currentJobPosition = jobPosition;
+    this.editMode = true;
+    this.isFormModalOpen = true;
+    console.log(jobPosition);
+  }
+
+  /**
+   * Add new Job Position
+   * @param jobPosition 
+   */
+  public addJobPosition(jobPosition: JobPosition): void {
+    this.dataService.postJobPosition(jobPosition);
+    this.jobPositionsCardData$ = this.dataService.getJobPositions()
+  }
+
+  /**
+   * Edit the current Job Position.
+   * @param jobPosition 
+   */
+  public editJobPosition(jobPosition: JobPosition): void {
+    this.dataService.editJobPosition(jobPosition)
+    this.jobPositionsCardData$ = this.dataService.getJobPositions()
+
+  }
+
+  public deleteJobPosition(jobPosition: JobPosition) {
+    if (jobPosition.id) {
+      this.dataService.deleteJobPosition(jobPosition.id)
+      this.jobPositionsCardData$ = this.dataService.getJobPositions()
+    }
+
+  }
+
+  // Method to close the Form Modal component
+  public closeFormModal() {
+    this.isFormModalOpen = false;
+  }
 }
