3a79e854f3a8017abb31a6da0c3071826a9d159c
Javier Casto Jarillo <javier.casto.jarillo@accenture.com>
2023-06-16 05:34:55 +0200

    add config for background image

--- src/app/components/pages/log/config/config.component.ts
+++ src/app/components/pages/log/config/config.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit, Renderer2 } from '@angular/core';
 import { ConfigService } from 'src/app/config.service';
 
 @Component({
@@ -9,7 +9,7 @@ import { ConfigService } from 'src/app/config.service';
 export class ConfigComponent implements OnInit {
   @HostBinding('style.--custom-title-color') customColor: string = 'lightseagreen';
 
-  constructor(private configService: ConfigService) { }
+  constructor(private configService: ConfigService, private renderer: Renderer2) { }
 
   ngOnInit() {
     this.configService.getSelectedTitleOption().subscribe(option => {
@@ -29,5 +29,22 @@ export class ConfigComponent implements OnInit {
 
   onTitleOptionChange(option: string) {
     this.configService.setSelectedTitleOption(option);
+  }
+
+  changeBackgroundImage(option: string) {
+    const body = document.getElementsByTagName('body')[0];
+    if (option === 'option1') {
+      this.renderer.setStyle(body, 'background-image', `url("/assets/images/background-img.png")`);
+    } else if (option === 'option2') {
+      // this.renderer.setStyle(body, 'background-image', 'url(/assets/images/3.jpg)');
+      this.renderer.setStyle(body, 'background-image', 'url(/assets/images/1234.jpg)');
+
+    }
+
+    else if (option === 'option3') {
+      this.renderer.setStyle(body, 'background-image', 'url(/assets/images/8.jpg)');
+      // this.renderer.setStyle(body, 'background-image', 'url(/assets/images/1234.jpg)');
+
+    }
   }
 }
